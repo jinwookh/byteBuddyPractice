d0cc1390cf4c7e4911f5f1d6fca12af8edfc0a6e
jinwooh <jin942002@naver.com>
2024-04-18 11:55:12 +0900

    init commit

---
+++ Masulsa.java
@@ -0,0 +1,5 @@
+public class Masulsa {
+    public static void main(String[] args) {
+        System.out.println(new Moja().pullOut());
+    }
+}
---
+++ Moja.java
@@ -0,0 +1,5 @@
+public class Moja {
+    public String pullOut() {
+        return "hi";
+    }
+}
